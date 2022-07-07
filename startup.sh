5685fac7dc1ee3440638525fba1562bccf1f4f27
Josiah M. Caprino <josiah.caprino@ecstech.com>
2022-07-07 16:24:00 +0000

    updates

--- startup.sh
+++ startup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-sudo apt install -y zsh trash-cli
+# Install zsh and trash-cli
+if ! command -v zsh &> /dev/null; then
+    sudo apt install -y zsh
+fi
+if ! command -v trash-put &> /dev/null; then
+    sudo apt install -y trash-cli
+fi
+
+# Install docker
+if ! command -v docker &> /dev/null; then
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sudo sh get-docker.sh
+fi
 
 # Install oh-my-zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -12,7 +24,7 @@ fi
 ln -sf $HOME/configs/.zshrc $HOME/.zshrc
 ln -sf $HOME/configs/functions.zsh $HOME/.oh-my-zsh/custom/functions.zsh
 ln -sf $HOME/configs/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
-ln -sf $HOME/configs/.zshrc $HOME/.zshrc
+ln -sf $HOME/configs/.vimrc $HOME/.vimrc
 
 # Setup git
 git config --global user.name "Josiah M. Caprino"
