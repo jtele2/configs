a24bfe89104b2c07ef21ae2666507409b612f221
jtele2 <14581506+jtele2@users.noreply.github.com>
2024-02-21 15:28:15 +0300

    updated folder to /home/ubuntu

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 # Setting up an Ubuntu Instance
 set -e -x
 
+DIR=/home/ubuntu
+
 setup_jq() {
 
   echo "SETTING UP JQ..."
@@ -28,8 +30,8 @@ setup_aws() {
 
     echo "SETTING UP AWS..."
     if ! command -v aws &> /dev/null; then
-        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
-        unzip $HOME/awscliv2.zip
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/home/ubuntu/awscliv2.zip"
+        unzip $DIR/awscliv2.zip
         sudo ./aws/install
         rm -rf aws
     fi
@@ -72,11 +74,15 @@ setup_zsh () {
     echo "SETTING UP ZSH..."
     apt-get update && apt-get install -y zsh wget # Install zsh and wget
     # Install oh-my-zsh
-    wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O $HOME/install.sh
-    chown -R ubuntu:ubuntu $HOME/install.sh
-    cd $HOME
+    wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O $DIR/install.sh
+    chown -R ubuntu:ubuntu $DIR/install.sh
+    cd $DIR
     echo pwd
-    runuser -l ubuntu -c 'sh install.sh'
+    if [ ! -d "$DIR/.oh-my-zsh" ]; then
+        runuser -l ubuntu -c 'sh install.sh'
+    else
+        echo "oh-my-zsh is already installed"
+    fi
     # Change the default shell to zsh
     chsh -s /usr/bin/zsh ubuntu
     rm -rf install.sh
@@ -86,25 +92,28 @@ setup_zsh () {
 setup_dotfiles () {
 
     echo "SETTING UP DOTFILES..."
-    git clone https://github.com/aws-samples/ec2-data-science-vim-tmux-zsh.git $HOME
-    ln -sf $HOME/configs/.vimrc $HOME/.vimrc
-    ln -sf $HOME/configs/.zshrc $HOME/.zshrc
-    ln -sf $HOME/configs/functions.zsh $HOME/.oh-my-zsh/custom/functions.zsh
-    ln -sf $HOME/configs/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
-    cp -asf $HOME/configs/completions $HOME/.zsh
-    chown -R ubuntu:ubuntu $HOME/configs $HOME/.vimrc $HOME/.zshrc $HOME/.oh-my-zsh/custom/functions.zsh $HOME/.oh-my-zsh/custom/aliases.zsh $HOME/.zsh
+    git clone https://github.com/jtele2/configs.git $DIR/configs
+    ln -sf $DIR/configs/.vimrc $DIR/.vimrc
+    ln -sf $DIR/configs/.zshrc $DIR/.zshrc
+    ln -sf $DIR/configs/functions.zsh $DIR/.oh-my-zsh/custom/functions.zsh
+    ln -sf $DIR/configs/aliases.zsh $DIR/.oh-my-zsh/custom/aliases.zsh
+    cp -asf $DIR/configs/completions $DIR/.zsh
+    chown -R ubuntu:ubuntu $DIR/configs $DIR/.vimrc $DIR/.zshrc $DIR/.oh-my-zsh/custom/functions.zsh /home/ubuntu/.oh-my-zsh/custom/aliases.zsh /home/ubuntu/.zsh
 
 }
-
-# get_dotfiles
-# setup_zsh
 
 # Check if a function name is passed as an argument
 if declare -f "$1" > /dev/null
 then
-  # Call the function passed as an argument
-  "$@"
+    # Call the function passed as an argument
+    "$@"
 else
-  echo "'$1' is not a known function name" >&2
-  exit 1
+    # Call all the functions
+    setup_jq
+    setup_unzip
+    setup_aws
+    setup_fzf
+    setup_trashcli
+    setup_zsh
+    setup_dotfiles
 fi
