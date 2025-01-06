66aace07681ff3293c5c54f394d179d3408217f1
Josiah M. Caprino <josiah.caprino@ecstech.com>
2025-01-06 15:26:01 +0000

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -3,56 +3,21 @@
 # TODO: Finish this file
 
 # Install jq
-if ! command -v jq &> /dev/null; then
-    sudo apt install -y jq
-fi
+brew install jq
 
 # Install unzip
-if ! command -v unzip &> /dev/null; then
-    sudo apt install -y zip
-fi
+brew install unzip
 
 # Install AWS CLI
-if ! command -v aws &> /dev/null; then
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
-    unzip $HOME/awscliv2.zip
-    sudo ./aws/install
-fi
-
-# Install Terraform
-if ! command -v terraform &> /dev/null; then
-    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-    sudo apt-get update && sudo apt-get install -y terraform
-fi
-
-# Install zsh and trash-cli
-if ! command -v zsh &> /dev/null; then
-    sudo apt install -y zsh
-fi
-if ! command -v trash-put &> /dev/null; then
-    sudo apt install -y trash-cli
-fi
+brew install awscli
 
 # Install docker
-if ! command -v docker &> /dev/null; then
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	sudo sh get-docker.sh >/dev/null
-    sudo usermod -aG docker $USER
-    newgrp docker
-fi
-
-# Install oh-my-zsh if not already installed
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
+brew install docker
 
 # Softlink the required files
 ln -sf $HOME/configs/.zshrc $HOME/.zshrc
 ln -sf $HOME/configs/functions.zsh $HOME/.oh-my-zsh/custom/functions.zsh
 ln -sf $HOME/configs/aliases.zsh $HOME/.oh-my-zsh/custom/aliases.zsh
-ln -sf $HOME/configs/.vimrc $HOME/.vimrc
 
 # Setup git
 git config --global user.name "Josiah M. Caprino"
